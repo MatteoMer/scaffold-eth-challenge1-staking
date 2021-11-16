6a14685632507c908c3aa5e8d7c0aa9676cd61a3
Matteo Mervoyer <mmervoyer@yolaw.fr>
2021-11-16 19:29:37 +0100

    Contract done

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,14 +1,59 @@
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity >=0.6.0;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
-
   ExampleExternalContract public exampleExternalContract;
 
-  constructor(address exampleExternalContractAddress) public {
+  constructor(address exampleExternalContractAddress) {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+  }
+
+  event Stake(address _from, uint256 _value);
+
+  mapping (address => uint256) public balances;
+  address[] private addrList;
+  uint256 public constant threshold = 1 ether;
+  uint256 public deadline = block.timestamp + 30 seconds;
+  bool private openForWithdraw = false;
+
+  function stake() payable public {
+    balances[msg.sender] += msg.value;
+    addrList.push(msg.sender);
+    emit Stake(msg.sender, msg.value);
+  }
+
+  function execute() public {
+    require(block.timestamp > deadline);
+    if (address(this).balance >= threshold){
+      exampleExternalContract.complete{value: address(this).balance}();
+      for (uint i=0; i < addrList.length ; i++){
+          balances[addrList[i]] = 0;
+        }
+      } else {
+      openForWithdraw = true;
+    }
+  }
+
+  function withdraw(address payable _addr) public payable {
+    require(balances[_addr] != 0);
+    require(openForWithdraw == true);
+    uint256 amount = balances[_addr];
+    balances[_addr] = 0;
+    _addr.transfer(amount);
+  }
+
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    } else {
+      return uint256(deadline - block.timestamp);
+    }
+  }
+
+  receive() external payable {
+    stake();
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
